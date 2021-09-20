1bc70a8fc4dd4a4fdfc0bb072932e6e48b29c78b
BittahCriminal <5335837+BittahCriminal@users.noreply.github.com>
2021-09-20 00:22:47 -0700

    init files

---
+++ Provisioning/Terraform/azureDeploy.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+      azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "=2.46.0"
+      }
+  }
+}
+variable "" {
+  
+}
+provider "azurerm" {
+    features {}
+}
